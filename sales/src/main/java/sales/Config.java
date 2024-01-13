b4c0eabef3c47cf356cfc824b707f0261cae7a72
molasisonfire <molasisonfire@gmail.com>
2024-01-12 22:02:35 -0300

    inject test

---
+++ sales/src/main/java/sales/Config.java
@@ -0,0 +1,12 @@
+package sales;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "sales system";
+    }
+}
