e998f2bb3d25a729990aac17ce77e9d070a85b43
molasisonfire <molasisonfire@gmail.com>
2024-01-19 16:35:55 -0300

    refactoring from controller to REST #2

--- sales/src/main/java/sales/rest/controller/CustomerController.java
+++ sales/src/main/java/sales/rest/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package sales.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import sales.model.Customer;
 import sales.repository.CustomerRepo;
 
@@ -17,34 +19,38 @@ public class CustomerController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity getCustomerById(@PathVariable Integer id) {
-        Optional<Customer> customer = customerRepo.findById(id);
-        if (customer.isPresent()) {
-            return ResponseEntity.ok(customer.get());
-        }
-        return ResponseEntity.notFound().build();
+    public Customer getCustomerById(@PathVariable Integer id) {
+        return customerRepo
+                .findById(id)
+                .orElseThrow( () -> new ResponseStatusException( HttpStatus.NOT_FOUND,"Not found"));
+
     }
 
     @PostMapping("/")
-    public ResponseEntity save(@RequestBody Customer customer) {
-        customerRepo.save(customer);
-        return ResponseEntity.ok().build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer save(@RequestBody Customer customer) {
+        return customerRepo.save(customer);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteCustomerById(@PathVariable Integer id) {
-        Customer c = new Customer(id, "test");
-        customerRepo.delete(c);
-        return ResponseEntity.ok().build();
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomerById(@PathVariable Integer id) {
+        customerRepo
+                .findById(id)
+                .map( found -> {
+                    customerRepo.delete(found);
+                    return null;
+                })
+                .orElseThrow( () -> new ResponseStatusException( HttpStatus.NOT_FOUND,"Not found"));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity update(@PathVariable Integer id, @RequestBody Customer customer) {
+    public Customer update(@PathVariable Integer id, @RequestBody Customer customer) {
         return customerRepo.findById(id).map(customerFound -> {
                     customer.setId(customerFound.getId());
                     customerRepo.save(customer);
-                    return ResponseEntity.noContent().build();
+                    return customerFound;
                 }
-        ).orElseGet(() -> ResponseEntity.ok().build());
+        ).orElseThrow(() -> new ResponseStatusException( HttpStatus.NOT_FOUND,"Not found"));
     }
 }
